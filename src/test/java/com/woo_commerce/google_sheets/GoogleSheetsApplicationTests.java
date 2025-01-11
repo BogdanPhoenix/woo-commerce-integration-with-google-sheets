2ee6bae36c6a9b2fc30f9d2a5848fdddcd462bc2
BogdanPhoenix <bohdan.musco@gmail.com>
2025-01-11 13:03:49 +0200

    Init project

---
+++ src/test/java/com/woo_commerce/google_sheets/GoogleSheetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woo_commerce.google_sheets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleSheetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
